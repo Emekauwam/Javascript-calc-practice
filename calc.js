8fb3d5fdadd422bc0cb495d720a75edac911aba5
Uwam Chukwuemeka <chukwuemekauwam@gmail.com>
2022-06-21 21:45:03 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,8 @@
+var selectOperationToPerform = alert('Hello, Please this calculator can only perform addition for now, other features will be added later, Thank you. Click ENTER');
+var num1 = prompt('Enter first number');
+var num2 = prompt ('enter another number');
+var num1 = parseInt(num1);
+var num2 = parseInt(num2);
+var sum = num1 + num2;
+document.write(alert(sum))
+document.write('<h1>Thank you for using my calculator, see you next time')
